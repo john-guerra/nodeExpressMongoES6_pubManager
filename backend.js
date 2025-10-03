76d521946f994e79313e0fd90993e7629ac545c2
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-10-03 11:58:54 -0700

    Adding a basic js frontend file

--- backend.js
+++ backend.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log("Initializing the backend server...");
+
+
 app.use(express.static("frontend"));
 
 app.listen(PORT, () => {
